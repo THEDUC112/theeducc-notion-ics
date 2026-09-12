71691700c8ab776ae18bc8ee0bd24b29393b3b62
theduc <107703138+THEDUC112@users.noreply.github.com>
2026-09-12 21:58:00 +0700

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,19 +2,14 @@ import { ICalEventBusyStatus } from 'ical-generator';
 import type { QueryDataSourceParameters } from '@notionhq/client/build/src/api-endpoints';
 
 export default {
-	filter: {
-		and: [
-			{ property: 'Status', status: { does_not_equal: 'Completed' } },
-			{ property: 'Status', status: { does_not_equal: 'Nope' } },
-			{ property: 'Type', select: { equals: 'Task' } }
-		]
-	},
-	dateProperty: 'Scheduled',
+	dateProperty: 'Date',
 	titleProperty: 'Name',
+	locationProperty: 'Place',
 	busy: ICalEventBusyStatus.FREE
 } as {
 	filter: Readonly<QueryDataSourceParameters['filter']>;
 	dateProperty: Readonly<string>;
 	titleProperty: Readonly<string>;
+	locationProperty: Readonly<string>;
 	busy: Readonly<ICalEventBusyStatus>;
 };
